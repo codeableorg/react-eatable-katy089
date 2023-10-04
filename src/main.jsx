411e16a1c32241d46ebd5754fc194369fd6d6dd5
katy089 <ladygetya@gmail.com>
2023-10-04 00:17:11 -0500

    one change

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/global/index'
+import { colors } from "./styles/global/colors";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
